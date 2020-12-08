3e3e439908ebe4987215771765b6a60615316486
ajcipolle <ajcipolle@gmail.com>
2020-12-07 21:45:31 -0800

    edit

--- static/js/map.js
+++ static/js/map.js
@@ -75,59 +75,60 @@ d3.json("/api/v1.0/fatal_counts_data").then(fatal_counts_data => {
     // d3.json("/api/v1.0/fill_blanks_data").then(fillblanks => {
 
     // Grab the fatal counts with d3 and flask
-    // d3.json("/api/v1.0/fatal_counts_data").then(fatal_counts_data => {
-    // console.log(fatal_counts_data)
-    // Grab the geoJSON country data
-    d3.json(link).then(data => {
-        console.log(data)
-            // Creating a geoJSON layer with the retrieved data
-        L.geoJson(data, {
-            // Style each feature (in this case a country)
-            style: function(feature) {
-                return {
-                    color: "black",
-                    // Call the chooseColor function to decide which color to color our country (color based on goals?)
-                    fillColor: chooseColor(feature.properties.ADMIN),
-                    fillOpacity: 0.5,
-                    weight: 0.5
-                };
-            },
-            // Called on each feature
-            onEachFeature: function(feature, layer) {
-                // Set mouse events to change map styling
-                layer.on({
-                    // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-                    mouseover: function(event) {
-                        layer = event.target;
-                        layer.setStyle({
-                            fillOpacity: .9,
-                            weight: 1,
-                            color: "white"
-                        });
-                    },
-                    // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-                    mouseout: function(event) {
-                        layer = event.target;
-                        layer.setStyle({
-                            fillOpacity: 0.5,
-                            weight: 0.5,
-                            color: "black"
-                        });
-                    },
-                    // When a feature (country) is clicked, it is enlarged to fit the screen
-                    click: function(event) {
-                        myMap.fitBounds(event.target.getBounds());
-                    }
-                });
-                // Conditional for matching geoJSON country names (feature.properties.ADMIN) with lat longs 
-                // Giving each feature a pop-up with information pertinent to it
-                //  `<h3>${feature.properties.ADMIN}</h3> <hr> <h4>fatal count: ${fatal_counts_data[0].fatal}!</h4>`
-                // run a function in the pop up to match geoJSON countries and results data
-                // layer.bindPopup(popuptext(feature.properties.ADMIN, autumnsdataapipull));
-                layer.bindPopup(`<h3>${feature.properties.ADMIN}</h3> <hr> <h4>fatal count: ${fatal_counts_data[0].fatal}!</h4>`);
+    d3.json("/api/v1.0/plot_data").then(plot_data => {
+        console.log(plot_data)
+            // Grab the geoJSON country data
+        d3.json(link).then(data => {
+            console.log(data)
+                // Creating a geoJSON layer with the retrieved data
+            L.geoJson(data, {
+                // Style each feature (in this case a country)
+                style: function(feature) {
+                    return {
+                        color: "black",
+                        // Call the chooseColor function to decide which color to color our country (color based on goals?)
+                        fillColor: chooseColor(feature.properties.ADMIN),
+                        fillOpacity: 0.5,
+                        weight: 0.5
+                    };
+                },
+                // Called on each feature
+                onEachFeature: function(feature, layer) {
+                    // Set mouse events to change map styling
+                    layer.on({
+                        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+                        mouseover: function(event) {
+                            layer = event.target;
+                            layer.setStyle({
+                                fillOpacity: .9,
+                                weight: 1,
+                                color: "white"
+                            });
+                        },
+                        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+                        mouseout: function(event) {
+                            layer = event.target;
+                            layer.setStyle({
+                                fillOpacity: 0.5,
+                                weight: 0.5,
+                                color: "black"
+                            });
+                        },
+                        // When a feature (country) is clicked, it is enlarged to fit the screen
+                        click: function(event) {
+                            myMap.fitBounds(event.target.getBounds());
+                        }
+                    });
+                    // Conditional for matching geoJSON country names (feature.properties.ADMIN) with lat longs 
+                    // Giving each feature a pop-up with information pertinent to it
+                    //  `<h3>${feature.properties.ADMIN}</h3> <hr> <h4>fatal count: ${fatal_counts_data[0].fatal}!</h4>`
+                    // run a function in the pop up to match geoJSON countries and results data
+                    // layer.bindPopup(popuptext(feature.properties.ADMIN, autumnsdataapipull));
+                    layer.bindPopup(`<h3>${feature.properties.ADMIN}</h3> <hr> <h4>fatal count: ${plot_data[0].prediction}!</h4>`);
 
-            }
-        }).addTo(myMap);
+                }
+            }).addTo(myMap);
+        });
     });
 });
 
